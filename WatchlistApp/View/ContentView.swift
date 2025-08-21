3b088ab9fcd665239e34722953c381a0ad6616f4
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-08-21 22:30:20 +0700

    List design updates. Add header. Fetched data is sorted.

--- WatchlistApp/View/ContentView.swift
+++ WatchlistApp/View/ContentView.swift
@@ -1,61 +1,86 @@
-//
-//  ContentView.swift
-//  WatchlistApp
-//
-//  Created by Anthony on 19/8/25.
-//
+  //
+  //  ContentView.swift
+  //  WatchlistApp
+  //
+  //  Created by Anthony on 19/8/25.
+  //
 
 import SwiftUI
 import SwiftData
 
 struct ContentView: View {
   @Environment(\.modelContext) private var modelContext
-  @Query private var movies: [Movie]
+  @Query(sort: \Movie.title) private var movies: [Movie] // alphabet order
   @State private var isSheetPresented = false
-
-    var body: some View {
-      List {
-        ForEach(movies) { movie in
-          HStack {
-            Text(movie.title)
-            Spacer()
-            Text(movie.genre.name)
-          } //: LIST ROW
-          .swipeActions {
-            Button (role: .destructive) {
-              withAnimation {
-                modelContext.delete(movie)
+  
+  var body: some View {
+    List {
+      if !movies.isEmpty {
+        Section {
+          ForEach(movies) { movie in
+            HStack {
+              Text(movie.title)
+                .font(.title)
+                .fontWeight(.light)
+                .padding(.vertical, 2)
+              Spacer()
+              Text(movie.genre.name)
+                .font(.footnote)
+                .fontWeight(.medium)
+                .padding(.horizontal, 6)
+                .padding(.vertical, 3)
+                .background(Capsule().stroke(lineWidth: 3))
+                .foregroundStyle(.tertiary)
+            } //: LIST ROW
+            .swipeActions {
+              Button (role: .destructive) {
+                withAnimation {
+                  modelContext.delete(movie)
+                }
+              } label: {
+                Label("Delete", systemImage: "trash")
               }
-            } label: {
-              Label("Delete", systemImage: "trash")
+            }
+          }
+        } header: {
+          VStack {
+            Text("Watchlist")
+              .font(.largeTitle)
+              .fontWeight(.black)
+              .padding()
+            HStack {
+              Label("Title", systemImage: "movieclapper")
+              Spacer()
+              Label("Genre", systemImage: "tag")
             }
           }
         }
-      } //: LIST
-      .overlay {
-        if movies.isEmpty {
-          EmptyListView()
-        }
       }
-      .safeAreaInset(edge: .bottom, alignment: .center) {
-        Button {
-          isSheetPresented.toggle()
-        } label: {
-          ButtonImageView(symbolName: "plus.circle.fill")
-        }
-      }
-      .sheet(isPresented: $isSheetPresented) {
-        NewMovieFormView()
+    } //: LIST
+    .overlay {
+      if movies.isEmpty {
+        EmptyListView()
       }
     }
+    .safeAreaInset(edge: .bottom, alignment: .center) {
+      Button {
+        isSheetPresented.toggle()
+      } label: {
+        ButtonImageView(symbolName: "plus.circle.fill")
+      }
+    }
+    .sheet(isPresented: $isSheetPresented) {
+      NewMovieFormView()
+    }
+  }
 }
 
 #Preview("Sample Data") {
   ContentView()
-    .modelContainer(Movie.preview)
+  .modelContainer(Movie.preview)
 }
 
 #Preview("Empty") {
-    ContentView()
-    .modelContainer(for: Movie.self, inMemory: true)
+  ContentView()
+  .modelContainer(for: Movie.self, inMemory: true)
 }
