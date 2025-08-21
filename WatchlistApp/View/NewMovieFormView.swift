ecdfd87cf97361d69252dd65f5dec8422066adb8
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-08-21 16:56:21 +0700

    Add Save and Cancel buttons

--- WatchlistApp/View/NewMovieFormView.swift
+++ WatchlistApp/View/NewMovieFormView.swift
@@ -10,6 +10,15 @@ import SwiftUI
 struct NewMovieFormView: View {
   @State private var title = ""
   @State private var selectedGenre: Genre = .scifi
+  @Environment(\.modelContext) var modelContext
+  @Environment(\.dismiss) var dismiss
+
+  private func addNewMovie() {
+    let newMovie = Movie(title: title, genre: selectedGenre)
+    modelContext.insert(newMovie)
+    title = ""
+    selectedGenre = .scifi
+  }
 
   var body: some View {
     Form {
@@ -18,7 +27,7 @@ struct NewMovieFormView: View {
         Text("What to Watch")
           .font(.largeTitle)
           .fontWeight(.black)
-          .foregroundStyle(.purple)
+          .foregroundStyle(.purple.gradient)
           .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
           //MARK: - TITLE
         TextField("Movie Title", text: $title)
@@ -33,9 +42,25 @@ struct NewMovieFormView: View {
           }
         }
           //MARK: - SAVE BUTTON
-
+        Button {
+          addNewMovie()
+          dismiss()
+        } label: {
+          Text("Save")
+            .font(.title2)
+            .fontWeight(.medium)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.extraLarge)
+            .buttonBorderShape(.roundedRectangle)
+        }
           //MARK: - CANCEL BUTTON
-
+        Button {
+          dismiss()
+        } label: {
+          Text("Close")
+            .frame(maxWidth: .infinity)
+        }
       } //: LIST
       .listRowSeparator(.hidden)
     }
